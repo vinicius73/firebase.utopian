59b460eed07aa29e26a5ebbc986925c144cd7e39
Diego Hernandes <diego@hernandev.com>
2018-06-22 16:52:46 -0300

    core unit exports

---
+++ src/units/core/routes.js
@@ -0,0 +1,8 @@
+// import express router.
+import { Router } from 'express'
+
+// create a router instance for the unit.
+const router = new Router()
+
+// default router export.
+export default router
