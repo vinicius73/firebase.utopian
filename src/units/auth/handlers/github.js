48aee7338dfc4928983c688bb88e2452e4b61866
Diego Hernandes <diego@hernandev.com>
2018-06-28 06:15:28 -0300

    github handler for token / id verification

--- src/units/auth/handlers/github.js
+++ src/units/auth/handlers/github.js
@@ -1,7 +1,10 @@
 // import github client.
 import { GithubClient } from 'src/support/github/client'
+// import account model.
+import { Account } from 'src/domains/users/account'
 // lodash helpers.
 import { get } from 'lodash'
+import { HttpsError } from 'src/support/firebase/functions/handler/errors'
 
 /**
  * Handle Github token validation.
@@ -10,18 +13,40 @@ import { get } from 'lodash'
  * @param context
  * @return {Promise<Object>}
  */
-export const handler = (data, context) => {
+export const handler = async (data, context) => {
+  // get current user UID from required.
+  const uid = get(context, 'auth.uid', null)
+  // throw when not authenticated.
+  if (!uid) {
+    return Promise.reject(new HttpsError(
+      'unauthenticated',
+      'Authentication is required for validating a Github account.'
+    ))
+  }
+
   // extract token from request body.
   const token = get(data, 'token', null)
 
   // reject if no token present.
   if (!token) {
-    return Promise.reject(new Error('Invalid token provided.'))
+    return Promise.reject(new HttpsError(
+      'invalid-data',
+      'A Github token is required for the account validation.'
+    ))
   }
 
   // start a github client instance.
   const client = new GithubClient(token)
 
   // return the github account data.
-  return client.getAccount()
+  const githubAccount = await client.getAccount()
+
+  // factory the account data.
+  const account = new Account({ name: uid, github: githubAccount })
+
+  // await saving the account.
+  await account.update()
+
+  // resolve with the account data itself.
+  return Promise.resolve({ message: 'SUCCESS' })
 }
