ffa1ca149943a647d79268fd36a77a05e0a048db
Vinicius Reis <luiz.vinicius73@gmail.com>
2018-07-07 15:42:14 -0300

    allows to create a custom instance of the http client

--- src/support/http/client.js
+++ src/support/http/client.js
@@ -1,34 +1,46 @@
 // imports.
-import { get } from 'lodash'
+import { get, defaultsDeep } from 'lodash'
 // import axios
 import axios from 'axios'
 // node http and https agents.
 import { Agent } from 'https'
 
-// create a new http agent instance with keep-alive enabled.
-const httpAgent = new Agent({ keepAlive: true })
-
-// axios options.
-const options = { httpAgent: httpAgent, httpsAgent: httpAgent }
-
-// create an axios instance.
-const instance = axios.create(options)
-
-// // flatten success responses body.
+// flatten success responses body.
 const flattenSuccess = response => get(response, 'data', {})
-// // flatten error responses body.
-// const flattenError = error => Promise.reject(get(error, 'response.data', {}))
+// flatten error responses body.
 const flattenError = error => error
 
-// enable success and error interceptors.
-instance.interceptors.response.use(flattenSuccess, flattenError)
+/**
+ * @method factoryInstance
+ * @param  {Object}        [args={}]
+ * @return {AxiosInstance}
+ */
+const factoryInstance = (args = {}) => {
+  // create a new http agent instance with keep-alive enabled.
+  const httpAgent = new Agent({ keepAlive: true })
+  // axios options.
+  const options = defaultsDeep({}, args, { httpAgent, httpsAgent: httpAgent })
 
-// set application/json as the default content-type header.
-instance.defaults.headers.post['Content-Type'] = 'application/json'
+  // create an axios instance.
+  const instance = axios.create(options)
+
+  // enable success and error interceptors.
+  instance.interceptors.response.use(flattenSuccess, flattenError)
+
+  // set application/json as the default content-type header.
+  instance.defaults.headers.post['Content-Type'] = 'application/json'
+
+  return instance
+}
+
+const instance = factoryInstance()
 
 // named export.
 /** @type {AxiosInstance} */
 export const http = instance
+
+/** @type {Function: AxiosInstance} */
+export const factory = factoryInstance
 
 // default export.
 /** @type {AxiosInstance} */
